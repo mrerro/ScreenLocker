28b9c1a1ac5cc45802ff56380d91abd033712a63
mrerro <dyakov.hellvisionstudio@gmail.com>
2018-02-04 19:00:46 +0500

    add closeButton

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
 
@@ -15,10 +16,22 @@ public class Controller {
     private TextField sNumber;
     @FXML
     private TextField sSurname;
+    @FXML
+    private Button closeButton;
 
     //1017103
     @FXML
-    public void onButtonClick() {
-        btCheck.setText(db.checkforvalidity(Integer.parseInt(sNumber.getText()), sSurname.getText().toLowerCase()).toString());
+    private void onButtonClick() {
+        if (!"".equals(sNumber.getText()) && !"".equals(sSurname.getText())) {
+            btCheck.setText(db.checkforvalidity(Integer.parseInt(sNumber.getText()), sSurname.getText().toLowerCase()).toString());
+        }
+    }
+    @FXML
+    private void closeButtonAction(){
+        KeyHook.unblockWindowsKey();
+        // get a handle to the stage
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
     }
 }
