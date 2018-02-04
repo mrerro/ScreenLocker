34f74c50925499ea9354486ba056d79507b8fe34
mrerro <dyakov.hellvisionstudio@gmail.com>
2018-02-04 19:01:29 +0500

    add fullscreen and alweysontop

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
@@ -13,6 +14,10 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Check mazafaking database");
         primaryStage.setScene(new Scene(root, 1024, 768));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.setMaximized(true);
+        KeyHook.blockWindowsKey();
         primaryStage.show();
     }
 
