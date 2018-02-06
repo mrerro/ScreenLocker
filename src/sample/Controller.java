9ff33cb707078cb012dfcb3098e3f53a3c828b9d
mrerro <dyakov.hellvisionstudio@gmail.com>
2018-02-07 00:16:00 +0500

    добавлено 4 минутное сварачивание, при правильном доступе

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,6 +9,7 @@ public class Controller {
 
 
     private DbController db = new DbController();
+    private Stage stage;
 
     @FXML
     private Button btCheck;
@@ -17,21 +18,38 @@ public class Controller {
     @FXML
     private TextField sSurname;
     @FXML
+    private TextField numberSheets;
+    @FXML
     private Button closeButton;
 
     //1017103
     @FXML
     private void onButtonClick() {
-        if (!"".equals(sNumber.getText()) && !"".equals(sSurname.getText())) {
-            btCheck.setText(db.checkforvalidity(Integer.parseInt(sNumber.getText()), sSurname.getText().toLowerCase()).toString());
+        if (!"".equals(sNumber.getText()) && !"".equals(sSurname.getText()) && !"".equals(numberSheets.getText())) {
+            if (db.check_for_validity(Integer.parseInt(sNumber.getText()), sSurname.getText().toLowerCase())) {
+                db.update_access_data(Integer.parseInt(sNumber.getText()),Integer.parseInt(numberSheets.getText()));
+                //give_access();
+            }
         }
     }
+
     @FXML
-    private void closeButtonAction(){
+    private void closeButtonAction() {
         KeyHook.unblockWindowsKey();
         // get a handle to the stage
-        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage = (Stage) closeButton.getScene().getWindow();
         // do what you have to do
         stage.close();
+        System.exit(1);
+    }
+
+    private void give_access() {
+        stage = (Stage) closeButton.getScene().getWindow();
+        stage.hide();
+        final long start = System.currentTimeMillis();
+        final long duration = 4*60*1000;
+        while (System.currentTimeMillis() < start + duration) {
+        }
+        stage.show();
     }
 }
